ce9be815fcd9e0fc0f01ffc534f507ecad810dd8
Developer-Dennis <denniskipkogei11@gmail.com>
2022-09-28 17:29:32 +0300

    created a task tracker function using react js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react"
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id:1,
+      text: 'Doctors Appointment',
+      day:'Feb 5th at 2.30pm',
+      reminder:true,
+    },
+    {
+        id:3,
+        text:'Food  Shopping',
+        day:'Feb 5th at 2.30pm',
+        reminder:false,
+    },
+    {
+      id:2,
+      text:'Meeting at school',
+      day:'Feb 6th at 1.30pm',
+      reminder:true,
+    }
+  ])
+ 
+// Delete Task
+
+const deleteTask=(id) => {
+  setTasks(tasks.filter((task) => task.id !==id))
+}
+
+
   return (
-    <div className="App">
-      
+    <div className="container">
+      <Header />
+     {tasks.lenght > 0 ? < Tasks tasks={tasks}  onDelete={deleteTask }/>: "No Tasks To Show"}
     </div>
   );
+}
+
+Header.defaultProps = {
+  title: "Task Tracker"
 }
 
 export default App;
