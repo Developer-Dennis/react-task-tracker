f170f463949c9b0bcf72f85d810e63e0e5c6ed0b
Developer-Dennis <denniskipkogei11@gmail.com>
2022-09-29 12:18:25 +0300

    created  delete function for task tracker

--- src/App.js
+++ src/App.js
@@ -29,11 +29,21 @@ const deleteTask=(id) => {
   setTasks(tasks.filter((task) => task.id !==id))
 }
 
+ 
+// Toggle reminder  
+
+const toggleReminder = (id) => {
+  console.log(id)
+}
+
 
   return (
     <div className="container">
       <Header />
-     {tasks.lenght > 0 ? < Tasks tasks={tasks}  onDelete={deleteTask }/>: "No Tasks To Show"}
+     {tasks.length > 0 ? < Tasks tasks={tasks}  onDelete=
+     {deleteTask } onToggle={toggleReminder}/>: (
+     "No Tasks To Show"
+    )}
     </div>
   );
 }
